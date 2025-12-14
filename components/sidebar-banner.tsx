384e5def1b5f31c85daebbd0934fdb8b6576d70f
Kowyo <kowyo@outlook.com>
2025-12-14 23:45:55 +0800

    chore: move components outside of app

--- components/sidebar-banner.tsx
+++ components/sidebar-banner.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { YearSelector } from '@/app/ui/year-selector';
+import { YearSelector } from '@/components/year-selector';
 import { SidebarTabsDropdown } from 'fumadocs-ui/components/sidebar/tabs/dropdown';
 import { useMemo } from 'react';
 import { getSidebarTabs } from 'fumadocs-ui/utils/get-sidebar-tabs';
